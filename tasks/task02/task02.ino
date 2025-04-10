092a659b44627744b5f6680e4dac47a2e1c505d2
9nocap6 <dorofeevdorof69ev@yandex.ru>
2025-04-10 19:59:10 +0400

    init

--- tasks/task02/task02.ino
+++ tasks/task02/task02.ino
@@ -1,7 +1,52 @@
-void setup() {
+const int SENSOR_PIN = 18;
+const int NUM_SAMPLES = 1000;
 
+volatile uint32_t sampleIndex = 0;
+volatile uint32_t totalTime = 0;
+uint32_t intervals[NUM_SAMPLES];
+uint32_t lastCaptureTime = 0;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(SENSOR_PIN, INPUT);
+    attachInterrupt(digitalPinToInterrupt(SENSOR_PIN), handlePulse, HIGH);
+}
+
+void handlePulse() {
+    uint32_t currentTime = micros();
+    uint32_t timeDiff = currentTime - lastCaptureTime;
+    
+    if (sampleIndex < NUM_SAMPLES) {
+        intervals[sampleIndex] = timeDiff;
+        totalTime += timeDiff;
+        sampleIndex++;
+    }
+    
+    lastCaptureTime = currentTime;
 }
 
 void loop() {
+    if (sampleIndex >= NUM_SAMPLES) {
+        noInterrupts();
+        analyzeData();
+        sampleIndex = 0;
+        totalTime = 0;
+        interrupts();
+    }
+}
 
+void analyzeData() {
+    float average = totalTime / (float)NUM_SAMPLES;
+    float sumSquaredDiffs = 0;
+    
+    for (int i = 0; i < NUM_SAMPLES; i++) {
+        float diff = intervals[i] - average;
+        sumSquaredDiffs += diff * diff;
+    }
+    
+    Serial.print("Average: ");
+    Serial.print(average);
+    Serial.print(" us, Std Dev: ");
+    Serial.print(sqrt(sumSquaredDiffs / NUM_SAMPLES));
+    Serial.println(" us");
 }
--- tasks/task03/task03.ino
+++ tasks/task03/task03.ino
@@ -1,7 +1,41 @@
-void setup() {
+const int outPins[] = {3, 5, 6, 9, 10};
+const unsigned long pulseTimes[] = {10000, 1000, 500, 100, 50};
+volatile bool toggleStates[] = {LOW, LOW, LOW, LOW, LOW};
+unsigned long timeStampA, timeStampB, timeStampC, timeStampD, timeStampE;
 
+void setup() {
+  for (int i = 0; i < 5; i++) {
+    pinMode(outPins[i], OUTPUT);
+  }
+  unsigned long startMicros = micros();
+  timeStampA = startMicros;
+  timeStampB = startMicros;
+  timeStampC = startMicros;
+  timeStampD = startMicros;
+  timeStampE = startMicros;
 }
 
 void loop() {
+  unsigned long nowMicros = micros();
 
+  if (nowMicros - timeStampE >= pulseTimes[4]) {
+    PORTB ^= B00000100;
+    timeStampE = nowMicros;
+  }
+  if (nowMicros - timeStampD >= pulseTimes[3]) {
+    PORTB ^= B00000010;
+    timeStampD = nowMicros;
+  }
+  if (nowMicros - timeStampC >= pulseTimes[2]) {
+    PORTD ^= B01000000;
+    timeStampC = nowMicros;
+  }
+  if (nowMicros - timeStampB >= pulseTimes[1]) {
+    PORTD ^= B00100000;
+    timeStampB = nowMicros;
+  }
+  if (nowMicros - timeStampA >= pulseTimes[0]) {
+    PORTD ^= B00001000;
+    timeStampA = nowMicros;
+  }
 }
